d5d05400c68c1fef62f5bdec46b55c4e897b5319
stuja16 <fastglove+github@gmail.com>
2024-12-17 15:51:29 -0600

    Clarified comments & trimmed link

--- Day4/Day4Part2.py
+++ Day4/Day4Part2.py
@@ -2,7 +2,7 @@
 # https://adventofcode.com/2024/day/4#part2
 
 # Parts of code taken from the following reddit comment solution:
-# https://www.reddit.com/r/adventofcode/comments/1h689qf/comment/m0bsh3p/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+# https://www.reddit.com/r/adventofcode/comments/1h689qf/comment/m0bsh3p/
 # I was unsure how to do this efficiently as my initial idea was just to use regex and rotate the word search repeatedly,
 # but I was struggling with the diagonals. The code from u/i_have_no_buscuits provided a more concise solution and taught me
 # some new python strategies. I have also adapted it to fit my own general framework.
@@ -18,8 +18,8 @@ def findMASes(grid):
     count = 0
     for y, x in grid:
         if grid[y,x] == "A":
-            lr = grid.get((y-1,x-1),"")+grid.get((y+1,x+1),"")  # top-left & bot-right
-            rl = grid.get((y+1,x-1),"")+grid.get((y-1,x+1),"")  # top-right & bot-left
+            lr = grid.get((y-1,x-1),"")+grid.get((y+1,x+1),"")  # top-left to bot-right diag
+            rl = grid.get((y+1,x-1),"")+grid.get((y-1,x+1),"")  # top-right to bot-left diag
             count += (lr in {"MS", "SM"}) and (rl in {"MS", "SM"})  # Both diagonals must be "MAS"
     return count
 
